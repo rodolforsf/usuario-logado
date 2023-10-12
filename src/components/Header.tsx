dfa1aee0e70bafd8ef9f1eb183486701e72ffe9d
Rodolfo Faria <106396728+rodolforsf@users.noreply.github.com>
2023-10-12 14:33:44 -0300

    Atuliazação no Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,25 @@
+import { LoggedUserContext } from "@/contexts/LoggedUser";
+import { useContext } from "react";
+
 export const Header = () => {
+    const loggedUserCtx = useContext(LoggedUserContext);
+
+    const handleLogout = () => {
+        loggedUserCtx?.setName('');
+    }
+
     return (
         <header className="flex">
             <h1 className="text-3xl flex-1">Título da Página</h1>
-          
+            {loggedUserCtx?.name &&
+                <>
+                    <p className="mt-1">Usuário Logado: {loggedUserCtx?.name}</p>
+                    <button onClick={handleLogout} className="ml-3 font-bold block">Sair</button>
+                </>
+            }
+            {!loggedUserCtx || loggedUserCtx?.name === '' &&
+                <p>Usuário DESLOGADO</p>
+            }
         </header>
     );
 }
